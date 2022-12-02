e6e4a1b4f5567677f1b18133ec09185cceb0bb79
asus <duannxph21488@fpt.edu.vn>
2022-12-02 22:45:18 +0700

    Duan_Tao_Layout_Chuyen_Fragment_Voi_Nhau

---
+++ app/src/main/java/com/fpoly/duan1demo/fragment/HoaDonFragment.java
@@ -0,0 +1,43 @@
+package com.fpoly.duan1demo.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fpoly.duan1demo.R;
+
+public class HoaDonFragment extends Fragment {
+
+    public HoaDonFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static HoaDonFragment newInstance() {
+        HoaDonFragment fragment = new HoaDonFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hoa_don, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
