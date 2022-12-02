d359d0c2d317236fde8f3800c5fe02cf89d3d6f9
asus <duannxph21488@fpt.edu.vn>
2022-12-03 00:14:38 +0700

    Duan_TaoMh_KH_Chuc_Nang_Them_Sua_Xoa

---
+++ app/src/main/java/com/fpoly/duan1demo/activity/DonHangActivity.java
@@ -0,0 +1,16 @@
+package com.fpoly.duan1demo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fpoly.duan1demo.R;
+
+public class DonHangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_don_hang);
+    }
+}
---
+++ app/src/main/java/com/fpoly/duan1demo/database/KhachHangDAO.java
@@ -0,0 +1,133 @@
+package com.fpoly.duan1demo.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.fpoly.duan1demo.object.KhachHang;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public
+class KhachHangDAO {
+    public static final String TABLE_NAME = "KhachHang";
+    public static final String SQL_NGUOIDUNG = "CREATE TABLE KhachHang (" +
+            "soDienThoai INTEGER PRIMARY KEY," +
+            "hoTen TEXT NOT NULL," +
+            "email TEXT NOT NULL," +
+            "diaChi TEXT NOT NULL," +
+            "tienNo NUMBER NOT NULL," +
+            "tienDaMua NUMBER NOT NULL)";
+
+    private final SQLiteDatabase sqLiteDatabase;
+
+    public KhachHangDAO(Context context) {
+        Mydatabase mydatabase = new Mydatabase(context);
+        sqLiteDatabase = mydatabase.getWritableDatabase();
+    }
+
+    public long addKhachHang(KhachHang khachHang) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("hoTen", khachHang.getTen());
+        contentValues.put("soDienThoai", khachHang.getSoDienThoai());
+        contentValues.put("email", khachHang.getEmail());
+        contentValues.put("diaChi", khachHang.getDiaChi());
+        contentValues.put("tienNo", khachHang.getTienNo());
+        contentValues.put("tienDaMua", khachHang.getTienDaMua());
+        return sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+    }
+
+    public long updateKhacHang(KhachHang khachHang, String sdt) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("hoTen", khachHang.getTen());
+        contentValues.put("soDienThoai", khachHang.getSoDienThoai());
+        contentValues.put("email", khachHang.getEmail());
+        contentValues.put("diaChi", khachHang.getDiaChi());
+        contentValues.put("tienNo", khachHang.getTienNo());
+        contentValues.put("tienDaMua", khachHang.getTienDaMua());
+        return sqLiteDatabase.update(TABLE_NAME, contentValues, "soDienThoai = ?", new String[]{sdt});
+    }
+
+    public long updateTienNo(KhachHang khachHang, String sdt) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("tienNo", khachHang.getTienNo());
+        return sqLiteDatabase.update(TABLE_NAME, contentValues, "soDienThoai = ?", new String[]{sdt});
+    }
+
+
+    public long updateTien(double tienNo, double tienMua, String sdt) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("tienNo", tienNo);
+        contentValues.put("tienDaMua", tienMua);
+        return sqLiteDatabase.update(TABLE_NAME, contentValues, "soDienThoai = ?", new String[]{sdt});
+    }
+
+    public long deleteKhachHang(String sdt) {
+        return sqLiteDatabase.delete(TABLE_NAME, "soDienThoai = ?", new String[]{sdt});
+    }
+
+    public List<KhachHang> getAllKhachHang() {
+        List<KhachHang> list = new ArrayList<>();
+        String query = "Select * from " + TABLE_NAME;
+        Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                String ten = cursor.getString(1);
+                String soDienThoai = cursor.getString(0);
+                String email = cursor.getString(2);
+                String diaChi = cursor.getString(3);
+                double tienNo = cursor.getDouble(4);
+                double tienDaMua = cursor.getDouble(5);
+
+                KhachHang khachHang = new KhachHang(ten, email, soDienThoai, diaChi, tienNo, tienDaMua);
+                list.add(khachHang);
+                cursor.moveToNext();
+            }
+            cursor.close();
+        }
+        return list;
+    }
+
+    public KhachHang getKhachHangBySDT(String sdt) {
+        String query = "Select * from " + TABLE_NAME + " where soDienThoai = '" + sdt + "'";
+        Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+        KhachHang khachHang = null;
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            String ten = cursor.getString(1);
+            String soDienThoai = cursor.getString(0);
+            String email = cursor.getString(2);
+            String diaChi = cursor.getString(3);
+            double tienNo = cursor.getDouble(4);
+            double tienDaMua = cursor.getDouble(5);
+            khachHang = new KhachHang(ten, email, soDienThoai, diaChi, tienNo, tienDaMua);
+        }
+        cursor.close();
+        return khachHang;
+    }
+
+    public List<KhachHang> timKiemKhachHang(String text) {
+        List<KhachHang> list = new ArrayList<>();
+        String query = "Select DISTINCT * from " + TABLE_NAME + " where soDienThoai like '%" + text + "%' or hoTen like '%" + text + "%'";
+        Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                String ten = cursor.getString(1);
+                String soDienThoai = cursor.getString(0);
+                String email = cursor.getString(2);
+                String diaChi = cursor.getString(3);
+                double tienNo = cursor.getDouble(4);
+                double tienDaMua = cursor.getDouble(5);
+                KhachHang khachHang = new KhachHang(ten, email, soDienThoai, diaChi, tienNo, tienDaMua);
+                list.add(khachHang);
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+        return list;
+    }
+}
